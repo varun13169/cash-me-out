16ea3876c86f732ab835729738342433f4ab6453
Varun Kumar <varun13169@iiitd.ac.in>
2021-08-14 10:41:31 +0530

    handle corner cases and bonus Question

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 let billAmountDoc = document.querySelector('#bill-amount');
 let cashGivenOutDoc = document.querySelector('#cash-given-out');
+let billAmountWarn = document.querySelector('#bill-amt-warn');
+let cashGivenOutWarn = document.querySelector('#cash-given-warn');
 let noteHolder = document.querySelector('#note-holder');
 let billAmount = 0;
 let cashGivenOut = 0;
@@ -36,26 +38,57 @@ function onChangeHandler(e) {
 
     switch(targetId) {
         case 'bill-amount':
+            billAmountWarn.style.display = "None";
+            billAmountWarn.innerHTML = "";
+
             billAmount = e.target.value
-            if(billAmount != '') {
+            
+            if(parseInt(billAmount, 10) > 0) {
+                if(billAmount === '') {
+                    cashGivenOutDoc.value = 0;
+                    cashGivenOutDoc.disabled = true;
+                    noteHolder.innerHTML = "";
+                }
+                else {
+                    cashGivenOutDoc.disabled = false;
+                }
                 console.log(billAmount)
             }
-            console.log(billAmount)
+            else {
+                billAmountWarn.style.display = "block";
+                billAmountWarn.innerHTML = "Enter Non-Negative Bill Amount.";
+            }
+
             break;
 
         case 'cash-given-out':
-            cashGivenOut = parseInt(e.target.value, 10)
-            console.log(cashGivenOut)
-            notes.forEach(note => {
-                if(cashGivenOut >= note) {
-                    console.log(cashGivenOut, note, (cashGivenOut => note))
-                    note = parseInt(note)
-                    notesDenominationDict[note] = parseInt(cashGivenOut / note, 10);
-                    cashGivenOut = cashGivenOut - (notesDenominationDict[note] * note)
+            cashGivenOutWarn.style.display = "None";
+            cashGivenOutWarn.innerHTML = "";    
+            cashGivenOut = parseInt(e.target.value, 10);
+            balanceCash = cashGivenOut - parseInt(billAmount, 10);
+
+            if (cashGivenOut > 0) {
+                if (balanceCash >= 0) {
+                    notes.forEach(note => {
+                        if(balanceCash >= note) {
+                            note = parseInt(note)
+                            notesDenominationDict[note] = parseInt(balanceCash / note, 10);
+                            balanceCash = balanceCash - (notesDenominationDict[note] * note)
+                        }
+                    });
+
+                    refreshNotesDisp();
+                    console.log(notesDenominationDict)
                 }
-            });
-            refreshNotesDisp();
-            console.log(notesDenominationDict)
+                else {
+                    cashGivenOutWarn.style.display = "block";
+                    cashGivenOutWarn.innerHTML = "Cash Given Out Has to be more than Bill Amount.";
+                }
+            }
+            else {
+                cashGivenOutWarn.style.display = "block";
+                cashGivenOutWarn.innerHTML = "Enter Non-Negative Cash Value.";
+            }
             break;
 
     }
